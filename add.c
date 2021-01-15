3ade81d60b4a3649fa419fe225a0c1782a33bcfd
Andrei Radu <andreigaming32@gmail.com>
2021-01-15 14:04:21 +0200

    update

--- add.c
+++ add.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) { 
-	int a = (int)argv[0];
-	int b = (int)argv[1];
-	printf("%d", a + b);
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+	if (argc != 3)
+	{
+		printf("Foloseste ./add nr1 nr2");
+		return -1;
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a + b;
+	
+	printf("suma = %d", sum);
+	
 	return 0;
 }
